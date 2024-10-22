ee4334531a0aa326c04dc802627e066c7f4318e2
INFINIX <72700816+DevInfinix@users.noreply.github.com>
2024-10-22 09:47:44 +0000

    headers-fix: Hard code some more scripts to use the absolute path to the headers
    Signed-off-by: INFINIX <72700816+DevInfinix@users.noreply.github.com>

--- tools/build/feature/test-libcrypto.c
+++ tools/build/feature/test-libcrypto.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
-#include "../openssl/sha.h"
-#include "../openssl/md5.h"
+#include "../../../include/openssl/sha.h"
+#include "../../../include/openssl/md5.h"
 
 int main(void)
 {
